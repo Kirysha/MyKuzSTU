064ced3d209a482bf5a63174636e191140484b22
Кирилл <tkirill42@mail.ru>
2018-10-13 09:40:59 +0700

    schedule

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,83 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { FlatList, ActivityIndicator, Text, View, StyleSheet } from 'react-native';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+export default class FetchExample extends React.Component {
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { isLoading: true }
+    }
+
+    componentDidMount() {
+        return fetch('https://vkbotkuzstu.azurewebsites.net/api/groupSchedule/4552')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log("all", responseJson)
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.schedules,
+                }, function () {
+
+                });
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    //componentDidMount() {
+    //    return fetch('https://vkbotkuzstu.azurewebsites.net/api/groupSchedule/ÏÈá-162')
+    //        .then((response) => response.json())
+    //        .then((responseJson) => {
+
+    //            this.setState({
+    //                isLoading: false,
+    //                dataSource: responseJson
+    //            }, function () {
+
+    //            });
+
+    //        })
+    //        .catch((error) => {
+    //            console.error(error);
+    //        });
+    //}
+
+
+
+    render() {
+
+        if (this.state.isLoading) {
+            return (
+                <View style={{ flex: 1, padding: 20 }}>
+                    <ActivityIndicator />
+                </View>
+            )
+        }
+        console.log("react", this.state.dataSource)
+        return (
+            <View style={{ flex: 1, paddingTop: 20 }}>
+                <FlatList style={styles.fla}
+                    data={this.state.dataSource}
+                    renderItem={({ item }) => <Text style={styles.item}>{item.subject}</Text>}
+                />
+            </View>
+        );
+    }
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            paddingTop: 22
+        },
+        item: {
+            padding: 10,
+            fontSize: 18,
+            height: 44,
+        },
+        fla: {
+            backgroundColor: '#fff'
+        }
+    })
